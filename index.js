784f117a50e0b9c14aaba6c650efd6bc98cf6d34
Nikhil Sanjay Wagh <nikhilwagh.work@gmail.com>
2018-02-06 01:11:28 +0530

    Removed bug in launchRequest
    When the request is `launchRequest` instead of `IntentRequest` there is no `event.request.intent`, so it generated a 'attempt to access a null pointer exception`

--- index.js
+++ index.js
@@ -10,6 +10,7 @@
 
 'use strict';
 const Alexa = require('alexa-sdk');
+//const parser = require('amazon-date-parser');
 
 //=========================================================================================================================================
 //TODO: The items below this comment need your attention.
@@ -17,21 +18,37 @@ const Alexa = require('alexa-sdk');
 
 //Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
 //Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
-const APP_ID = 'amzn1.ask.skill.f1b08f7a-6261-48b6-aab8-f860662963aa';
+const APP_ID = 'amzn1.ask.skill.fe810233-189e-4721-a3bf-ad40ca9c9aa7';
 
 const SKILL_NAME = 'Contest Tracker';
-const GET_CONTEST_MESSAGE = "Here are some contests: ";
-const HELP_MESSAGE = 'You can say tell me about some upcoming contests, or, you can say exit... What can I help you with?';
-const HELP_REPROMPT = 'What can I help you with?';
+const GET_CONTEST_MESSAGE = 'Here are some contests: ';
+const HELP_REPROMPT = 'You can say tell me about some upcoming contests, or, you can say exit.';
+const HELP_MESSAGE = 'What can I help you with?';
 const STOP_MESSAGE = 'Goodbye! and happy coding';
+
 
 //=========================================================================================================================================
 //TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/lambda/data
 //=========================================================================================================================================
+var welcomeArr = [
+  'Welcome, ',
+  "Hello, ",
+  "Hello coder, ",
+  "Nice to see you, ",
+  "A lot of contests to rule today. "
+];
+var userPlatform = null;
+var userDate = null;
+var userTime = null;
+var whenDate = null;
+var whenTime = null;
+var found = 0;
+var ans = "", k = 0;
+var eMessage = "Not updated", invalidRequest = false;
 
 //=========================================================================================================================================
 //Editing anything below this line might break your skill.
-//=========================================================================================================================================
+//========================================================================================================================================
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -41,144 +58,63 @@ exports.handler = function(event, context, callback) {
 };
 const handlers = {
     'LaunchRequest': function () {
-        this.emit('GetNewContestIntent');
+        const Index = Math.floor(Math.random() * welcomeArr.length);
+        const randomWel = welcomeArr[Index];
+
+        this.emit(':ask',randomWel + HELP_MESSAGE);
+        //this.emit('GetNewContestIntent');
     },
     'GetNewContestIntent': function () {
         console.log('GetNewContestIntent');
+        ans = ""; k = 0; eMessage = "Not updated"; invalidRequest = false;
 
-        var errorMessage = "Unable to process the request, ", invalidRequest = false ;
-        var speechOutput = GET_CONTEST_MESSAGE;
+        var appId = this.event.session.application.applicationId;
+        if (appId != APP_ID){
+          var speechOutput = "Application ID didnot match.";
+          this.emit(':tell', speechOutput);
+          return http.console.error(400); //TODO: search the correct syntax
+        }
+
+
         //slots
-        var userPlatform = this.event.request.intent.slots.Platform.value;
-        var userDate = this.event.request.intent.slots.Date.value;
-        var userTime = this.event.request.intent.slots.Time.value;
-        var whenDate = this.event.request.intent.slots.whenDate.value;
-        var whenTime = this.event.request.intent.slots.whenTime.value;
-
-        console.log("Data given by the user : ", userPlatform, whenDate, userDate, whenTime, userTime);
-
-        
-        function whenValue(when){
-            switch(when){
-                case 'on': return 1;
-                case 'after': return 2;
-                case 'before': return 3;
-                default: return -1;
-            }
-        }
-        
-        function updateAns(entry){
-            var ans = entry.Name + " on " + entry.Platform + " starts at " + entry.StartTime + " and ends at " + entry.EndTime + "\n";
-            console.log("ANS:", ans);
-            this.response.speak(ans);
-            this.emit(':responseReady');
+        if (this.event.request.intent != null) {
+          userPlatform = this.event.request.intent.slots.Platform.value;
+          userDate = this.event.request.intent.slots.Date.value;
+          userTime = this.event.request.intent.slots.Time.value;
+          whenDate = this.event.request.intent.slots.whenDate.value;
+          whenTime = this.event.request.intent.slots.whenTime.value;
         }
 
-        function forTimedoThis(entry, time){
-            var dateTime = new Date(Date.parse(entry.StartTime));
-            if(time){
-                switch(whenValue(whenTime)){
-                    case 1: 
-                        if(dateTime.getTime() == userTime){
-                            updateAns(entry);
-                        }
-                        break;
-                    case 2: 
-                        if(dateTime.getTime() > userTime){
-                            updateAns(entry);
-                        }
-                        break;
-                    case 3:
-                        if(dateTime.getTime() < userTime){
-                            updateAns(entry);
-                        }
-                        break;
-                    default: 
-                        invalidRequest = true;
-                        errorMessage += " , time not recognised, please repeat it for me."
-                        console.log("Error: whenTime not recognised");
-                        return -1;//whenTime not recognised
-                }
+        console.log("User Data:", userPlatform, whenDate, userDate, whenTime, userTime);
+
+        //check for valid user name
+        if (userPlatform != null) {
+          userPlatform = userPlatform.toString().trim().toUpperCase().replace(/ /g,'');
+          if (!isValidPlatform(userPlatform)) {
+            var prompt = "Sorry " + userPlatform + " is not supported yet. Please try again with our supported platforms, " +
+            "which are <break time=\"200ms\"/> codeforces, <break time=\"200ms\"/> codechef, <break time=\"200ms\"/> topcoder, " +
+            "<break time=\"200ms\"/> google code jam, <break time=\"200ms\"/> hackerrank and <break time=\"200ms\"/> hackerearth.";
+            var reprompt = "Do you wish to continue?";
+
+            this.emit(':tell', removeSSML(prompt));
+            this.emit(':ask', removeSSML(reprompt));
+            if(true){               // TODO: take response from user and do the necessities
+              //TODO: return new userPlatform
             }else{
-                updateAns(entry);
+              //TODO: exit skill
             }
-            return 0;
+          }
         }
 
-        function forDatedoThis(entry, date, time){
-            var dateTime = new Date(Date.parse(entry.StartTime));
-            if(date){
-                switch(whenValue(whenDate)){
-                    case 1: // on
-                        {
-                            if(dateTime.getDate() == Date(userDate))
-                            {
-                                forTimedoThis(entry, time);
-                            }
-                        }
-                        break;
-                    case 2: // after
-                        {
-                            if(dateTime.getDate() > Date(userDate))
-                            {
-                                forTimedoThis(entry, time);
-                            }
-                        }
-                        break;
-                    case 3: // before
-                        {
-                            if(dateTime.getDate() < Date(userDate))
-                            {
-                                forTimedoThis(entry, time);
-                            }
-                        }
-                        break;
-                    default: 
-                        invalidRequest = true;
-                        errorMessage += ", date not recognised, please repeat it for me.";
-                        console.log("Error: whenDate not recognised");
-                        return -1; //whenDate not recognised
-                }
-            }else{
-                forTimedoThis(entry, time);
-            }
-            return 0;
+        //check for correct syntax of date and when
+        if ((userDate != null) != (whenDate != null)) {
+          whenDate = "on";
+        }
+        //check for correct syntax of time and when
+        if ((userTime != null) != (whenTime != null)) {
+          whenTime = "on";
         }
 
-        function isPlatform(platform){
-            switch(platform){
-                case "CODEFORCES" : 
-                case "CODECHEF" : 
-                case "GOOGLE CODE JAM" : 
-                case "HACKEREARTH": 
-                case "HACKERRANK" : 
-                case "TOP CODER" : return true; 
-                default : return false;
-            }
-        }
-
-        function forPlatformdoThis(entry, platform, date, time){
-            if(platform){
-                if(isPlatform(userPlatform)){
-                    if (entry.Platform == userPlatform) {
-                        forDatedoThis(entry, date, time);
-                    }
-                }else{
-                    invalidRequest = true;
-                    errorMessage = "Sorry, We don't have access to this platform yet."
-                }
-            } else{
-                forDatedoThis(entry, date, time);
-            }
-        }
-
-        var options = {
-            host: 'http://contesttrackerapi.herokuapp.com/',
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
 
         const http = require('http');
         http.get('http://contesttrackerapi.herokuapp.com/', (res) => {
@@ -205,18 +141,43 @@ const handlers = {
             res.on('end', () => {
                 try {
                     const parsedData = JSON.parse(rawData);
-                    console.log(parsedData);
+                    console.log("Acquired results from remote site.");
+                    //console.log(parsedData);
                     parsedData.result.upcoming.forEach(function(entry){
                         forPlatformdoThis(entry, userPlatform != null, userDate != null, userTime != null);
-                    })
+                    });
+
+                    if(invalidRequest == true){
+                        console.log("Error:", eMessage);
+                        this.response.speak(eMessage);
+                        this.emit(':responseReady');
+                    }else{
+                        //console.log("Found", k.toString(), "results");
+                        var speechOutput;
+                        if (k == 0) {
+                            console.log("Ans:", ans);
+                            speechOutput = "Sorry no results found for your search. Try generalising your request or may be ask for some other platform";
+                        }else{
+                            speechOutput = "Found " + k.toString() + " result" + ((k > 1)?"s":"") + ". <break time=\"500ms\"/>";
+                            speechOutput += ans;
+                        }
+                        //console.log(removeSSML(speechOutput));
+                        this.emit(':tellWithCard', speechOutput, SKILL_NAME, removeSSML(speechOutput));
+                        this.emit(':responseReady');
+                        /*this.response.speak(speechOutput);
+                        this.emit(':responseReady');*/
+                    }
                 } catch (e) {
                     console.error(e.message);
+                    this.response.speak("Sorry, something went wrong. Please try again");
+                    this.emit(':responseReady');
                 }
             });
             }).on('error', (e) => {
             console.error(`Got error: ${e.message}`);
+            this.response.speak("Sorry, something went wrong. Please try again");
+            this.emit(':responseReady');
         });
-
     },
     'AMAZON.HelpIntent': function () {
         const speechOutput = HELP_MESSAGE;
@@ -233,4 +194,155 @@ const handlers = {
         this.response.speak(STOP_MESSAGE);
         this.emit(':responseReady');
     },
+    'SessionEndedRequest': function () {
+        this.emit(':tell', STOP_MESSAGE);
+        // this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    }
 };
+//yyyy-mm-dd
+function getDate(s){
+    var date = s.substr(5,2);
+    var month = s.substr(8,3);
+    var year = s.substr(12, 4);
+    var mAll = ["Jan","Feb","Mar","April","May","June","July","Aug","Sep","Oct","Nov","Dec"];
+    var i;
+    for (i = 0; i < mAll.length; i++) {
+        if (mAll[i] == month)
+        break;
+    }
+    var result = year + "-" + (i<9?"0":"") + (i+1).toString() + "-" + date;
+    return result.toString();
+}
+function getTime(s){
+    return s.substr(17, 5);
+}
+
+function removeSSML (s) {
+    return s.replace(/<\/?[^>]+(>|$)/g, "");
+}
+
+function isValidPlatform(platform){
+    switch(platform){
+        case "CODEFORCES" :
+        case "CODECHEF" :
+        case "GOOGLECODEJAM" :
+        case "HACKEREARTH":
+        case "HACKERRANK" :
+        case "TOPCODER" : return true;
+        default : return false;
+    }
+}
+
+
+function whenValue(when){
+    switch(when){
+        case 'on': return 1;
+        case 'after': return 2;
+        case 'before': return 3;
+        default: return -1;
+    }
+}
+
+function updateAns(entry){
+    var value = entry.Name + " on " + entry.Platform + " starts at " + entry.StartTime + " for the duration of " + entry.Duration + " . \n <break time=\"500ms\"/>";
+    ans += value;
+    k++;
+    console.log("User Input: ", userPlatform, whenDate, userDate, whenTime, userTime );
+    console.log("Extracted data: ", entry.Platform, entry.StartTime, entry.Duration);
+}
+
+function forTimedoThis(entry, time){
+    //console.log("forTimedoThis:", time);
+    if(time){
+        var fechedTime = getTime(entry.StartTime);
+        //console.log(fechedTime, userTime);
+        switch(whenValue(whenTime)){
+            case 1:
+                if(fechedTime == userTime){
+                    updateAns(entry);
+                }
+                break;
+            case 2:
+                if(fechedTime > userTime){
+                    updateAns(entry);
+                }
+                break;
+            case 3:
+                if(fechedTime < userTime){
+                    updateAns(entry);
+                }
+                break;
+            default:
+                // TODO: check this block
+                //whenTime not recognised
+                console.log("Error: whenTime not recognised");
+                eMessage = "You need to specify before, on or after of the time, " + userTime;
+                invalidRequest = true;
+                /*this.response.speak("You need to specify before, on or after of the time,", userTime);
+                this.emit(':responseReady');*/
+        }
+    }else{
+        updateAns(entry);
+    }
+    return 0;
+}
+
+
+function forDatedoThis(entry, date, time){
+    console.log("forDatedoThis:", date, time);
+    if(date){
+        var fechedDate = getDate(entry.StartTime);
+        // var dt = new parser(userDate);
+        // var date = dt.startDate.toLocaleDateString('en-US');
+        console.log("Fetched:", fechedDate, "UserInput:", userDate);
+        switch(whenValue(whenDate)){
+            case 1: // on
+                {
+                    if(fechedDate == userDate)
+                    {
+                        forTimedoThis(entry, time);
+                    }
+                }
+                break;
+            case 2: // after
+                {
+                    if(fechedDate > userDate)
+                    {
+                        forTimedoThis(entry, time);
+                    }
+                }
+                break;
+            case 3: // before
+                {
+                    if(fechedDate < userDate)
+                    {
+                        forTimedoThis(entry, time);
+                    }
+                }
+                break;
+            default:
+                //TODO: check this block
+                //whenDate not recognised
+                console.log("Error: whenDate not recognised");
+                eMessage = "You need to specify before, on or after of the date," + userDate;
+                invalidRequest = true;
+                /*this.response.speak("You need to specify before, on or after of the date,", userDate);
+                this.emit(':responseReady');*/
+        }
+    }else{
+        forTimedoThis(entry, time);
+    }
+    return 0;
+}
+
+function forPlatformdoThis(entry, platform, date, time){
+    //console.log("forPlatformdoThis:", platform, date, time);
+    if(platform){
+        if (entry.Platform.toString().trim() == userPlatform) {
+            forDatedoThis(entry, date, time);
+        }
+    } else{
+        forDatedoThis(entry, date, time);
+    }
+}
